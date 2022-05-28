23a59348be828e98c3f1126826f11a81ac8648e7
notaCoder15 <103276216+notaCoder15@users.noreply.github.com>
2022-05-28 06:40:30 +0530

    Update Voting.sol

--- Voting.sol
+++ Voting.sol
@@ -6,29 +6,31 @@ import "./GovTok.sol";
 import "./globalVar.sol";
 
 // Voting Contract
-
+// New contract for new praposal
 
 contract voting is GovernanceToken , GlobVars{
 
-    uint totalSupply;
-    uint newValue1;
-    uint newValue2;
-    uint deadline;
+    uint totalsupply;                       // max number of votes
+    uint newValue1;                         // new value of Subscription Fee
+    uint newValue2;                         // new value of deconfirmations required
+    uint deadline;                          // deadline timestamp for voting (origin + 1 day)
 
-    mapping (address => bool) isVoteCasted;
-    uint forThePraposal;
+    mapping (address => bool) isVoteCasted;  // check if the subscriber has casted vote of not
+    uint forThePraposal;                     // total number of votes in favor of the praposals
 
-    IGovernance Governance;
+    IGovernance governance;                 
 
+// function to setup the contract . Called while initializing the contract in Governance . Can be called only once.
     function setup(uint amount1, uint amount2 , uint _totalSupply) public {
-        require(address(Governance) == address(0) , "Setup already done");
-        Governance = IGovernance(msg.sender);
-        totalSupply = _totalSupply;
+        require(address(governance) == address(0) , "Setup already done");
+        governance = IGovernance(msg.sender);
+        totalsupply = _totalSupply;
         newValue1 = amount1;
         newValue2 = amount2;
         deadline = block.timestamp + 1 days;
     }
-
+// function to cast vote
+// used before deadline is over
     function castVote(bool vote) public onlyAllowed {
         require(isVoteCasted[msg.sender] == false , "Vote already casted");
         require(block.timestamp < deadline , "Deadline Passed");
@@ -38,10 +40,11 @@ contract voting is GovernanceToken , GlobVars{
         }
         isVoteCasted[msg.sender] = true;
     }
-
+    // function to get the result of the voting
+    // can be called after deadline is over
     function result() public view returns(uint,uint , bool){
         require(block.timestamp > deadline, "Voting still going on");
-        uint percent = (forThePraposal/totalSupply)*100;
+        uint percent = (forThePraposal/totalsupply)*100;
         if(percent >= 50){
             return (newValue1, newValue2 ,true);
         }else{
