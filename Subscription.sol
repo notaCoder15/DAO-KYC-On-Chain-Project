33f291ed7f62e0fe29a8d60791fb1911d841003c
notAcoder <akblockchain15@gmail.com>
2022-05-28 06:13:27 +0530

    made changes

--- Subscription.sol
+++ Subscription.sol
@@ -1,28 +1,35 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.10;
 
-contract Subscription{
+import "./globalVar.sol";
 
-    event FiSubscriptonBought(address indexed _user , uint32 _timestamp);
+/* This contract is chaincoded into the private network . Users must buy the subscription 
+    to enter the network and use the kyc chain*/
 
+contract Subscription is GlobVars{
 
-
-    uint public fiSubscription;
-
-
-    mapping(address => bool) public isFiSubscriber;
+    event SubscriptonBought(address indexed _user , uint32 _timestamp);
+    event FeeChanged(uint indexed newVal);
 
     constructor(){
-
-        fiSubscription = 1 ether;
+        SubscriptionFee = 1 ether;        // Sets the initial subscription fee to 1 ether . Can be changed via Governance protocol.
     }
 
 
 
-    function becomeFiSub() public payable {
-        require(msg.value == fiSubscription , "Not right amount of fee to get Financial Institution Subscription");
-        isFiSubscriber[msg.sender] = true;
-        emit FiSubscriptonBought(msg.sender, uint32(block.timestamp));
+    // Function to become a subscriber to use the chain and contribute to it.
+    function becomeSub() public payable {
+        require(msg.value == SubscriptionFee , "Not right amount of fee to get Financial Institution Subscription");
+        require(isSubscriber[msg.sender] == false , "already subscribed");
+
+        isSubscriber[msg.sender] = true;
+        emit SubscriptonBought(msg.sender, uint32(block.timestamp));
     }
 
+    // function to change subscription fee
+    // can only be called in governance contract
+    function changeSubscriptionFee(uint newVal) internal {
+        SubscriptionFee = newVal;
+        emit FeeChanged(newVal);
+    }
 }  
