d5c3c5b31c9602decbb6fda8403ea6c9be70a722
notAcoder <akblockchain15@gmail.com>
2022-05-28 06:15:58 +0530

    made changes

--- checkKyc.sol
+++ checkKyc.sol
@@ -5,9 +5,13 @@ import "./AddKyc.sol";
 
 contract CheckKyc is AddKyC {
 
+    // functions that checks wheather a user's KYC has been completed or not
     function isUserPresent(uint _id) public view returns(bool){
         return isPresent[_id];
     }
+
+    // function to retrive on chain information about the user
+    // function returns encoded string that can be decoded using user's private key
 
     function getInfo(uint _id) public view onlyAllowed returns (uint id , string memory info1 , string memory info2 , address adder , uint numOfconf){
         id = _id;
@@ -16,6 +20,8 @@ contract CheckKyc is AddKyC {
         adder = Users[_id].kycadder;
         numOfconf = Users[_id].numOfConfirmations;
     }
+
+    // Function for other Financial Institutions to confirm wheather the Kyc performed was correct or wrong
 
     function confirm(uint _id , bool _valid) public onlyAllowed {
         require(Confirmed[_id][msg.sender] ==  false , "vote already given");
